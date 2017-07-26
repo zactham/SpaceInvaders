0940807cf49f39e49062c35026dd900922effc35
Zac Thamer <zactham@gmail.com>
2017-07-26 00:27:32 -0400

    New alien1 and player class

--- src/Alien1.java
+++ src/Alien1.java
@@ -1,16 +1,17 @@
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class Player extends GameObject
+public class Alien1 extends GameObject
 {
+	@Override
 	public GameObjectType getType()
 	{
-		return GameObjectType.Player;
+		return GameObjectType.Alien1;
 	}
 	
 	public void draw(Graphics page, Color outline)
 	{
 		page.setColor(Color.red);
-		page.drawRect(getX(), getY(), getStandardSize(), getStandardSize());
+		page.drawRect(getX(), getY(),getStandardSize(), getStandardSize());
 	}
 }
