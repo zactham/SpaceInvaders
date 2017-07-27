438450ad40326089659ce49efd3cf5d3edc9c209
Zac Thamer <zactham@gmail.com>
2017-07-27 01:10:43 -0400

    Player class: has a direction w acessor and modifiers, update method moves x right and left based on the direction

--- src/Player.java
+++ src/Player.java
@@ -3,6 +3,10 @@ import java.awt.Graphics;
 
 public class Player extends GameObject
 {
+	private PlayerDirection direction = PlayerDirection.NONE;
+	private int speed = 2;
+	
+	
 	@Override
 	public GameObjectType getType()
 	{
@@ -14,5 +18,27 @@ public class Player extends GameObject
 	{
 		page.setColor(Color.red);
 		page.drawRect(getX(), getY(), getStandardSize(), getStandardSize());
+	}
+	
+	public PlayerDirection getDirection()
+	{
+		return direction;
+	}
+	
+	public void setDirection(PlayerDirection d)
+	{
+		direction = d;
+	}
+	
+	@Override
+	public void update()
+	{
+		super.update();
+		
+		if (direction == PlayerDirection.RIGHT)
+			x+=speed;
+		
+		if (direction == PlayerDirection.LEFT)
+			x-=speed;
 	}
 }
